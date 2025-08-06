d6e5a546d454ec3bebd8b9e35cb805082fe53dcd
apmoskal <moskalap@bc.edu>
2025-08-05 22:56:10 -0400

    Update streamlit_app(1).py

--- streamlit_app(1).py
+++ streamlit_app(1).py
@@ -9,8 +9,3 @@ st.markdown("<h2 style='font-size:2rem; margin-bottom: 1rem;'>Crops, Countries, 
 st.image("crops-growing-in-thailand.jpg", width=600)
 
 st.header("Basic Crop Information")
-columns_needed = ['Item', 'Plant Climate', 'Water Needs', 'Sunlight', 'Average Optimal Temp', 'Crop Type', 'Common Uses']
-if all(col in df.columns for col in columns_needed):
-    st.dataframe(df[columns_needed].drop_duplicates().reset_index(drop=True), use_container_width=True)
-else:
-    st.info("Some required columns are missing in the dataset.")
